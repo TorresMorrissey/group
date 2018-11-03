8dc57852c35b35b5799c3fba00f40b74b0dbd2c1
Campbellim <Campbellim18@gmail.com>
2018-11-03 13:39:53 -0700

    ajaxcall

--- assets/javascript/nytCode.js
+++ assets/javascript/nytCode.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    
-}):
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "a617980394da4c8592d6328db5533de0",
+      'q': "guns",
+      //'begin_date': "",
+      //'end_date' : "",
+      //'hl' : "",
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result.response.docs)
+    }).fail(function(err) {
+      throw err; 
+    });
+});
